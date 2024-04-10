cb4faea4ec3915d53b3bdaf5ad3889f607f5a937
Alys9999 <74774427+Alys9999@users.noreply.github.com>
2024-04-10 14:15:56 -0700

    1

---
+++ lambda/presignHandler.ts
@@ -0,0 +1,34 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import {getSignedUrl } from "@aws-sdk/s3-request-presigner"
+
+exports.handler = async (event: any) => {
+    const { fileName, fileType } = JSON.parse(event.body);
+    console.log(fileType);
+    const client = new S3Client({ region: process.env.AWS_REGION });
+
+    const command = new PutObjectCommand({
+        Bucket: process.env.BUCKET_NAME,
+        Key: fileName,
+        ContentType: fileType,
+        ACL: 'private',
+    });
+
+    try {
+
+        const presignedUrl = await getSignedUrl(client, command, { expiresIn: 60 });
+        console.log(presignedUrl);
+        return {
+            statusCode: 200,
+            headers: {"Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*", 
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",}, 
+            body: JSON.stringify({ url: presignedUrl }),
+        };
+    } catch (e) {
+        console.log(e);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Could not generate a presigned URL" }),
+        };
+    }
+};
--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -1,16 +1,78 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_dynamodb as dynamodb, aws_s3 as s3, aws_iam as iam, aws_apigateway as apigateway, aws_lambda as lambda, aws_ec2 as ec2 } from 'aws-cdk-lib';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const fileBucket = new s3.Bucket(this, 'FileBucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,  
+      removalPolicy: cdk.RemovalPolicy.DESTROY, 
+      cors:[
+        {
+          allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.PUT, s3.HttpMethods.POST],
+          allowedOrigins: ['*'],
+          allowedHeaders: ['*'],
+          maxAge: 3000,
+        }
+      ]
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    const fileTable = new dynamodb.Table(this, 'FileTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, 
+    });
+
+    const lambdaExecutionRole = new iam.Role(this, 'LambdaExecutionRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaVPCAccessExecutionRole'),
+      ],
+    });
+    lambdaExecutionRole.addToPolicy(new iam.PolicyStatement({
+      resources: [fileBucket.bucketArn, fileTable.tableArn],
+      actions: ['s3:*', 'dynamodb:*'],
+    }));
+
+
+
+    const api = new apigateway.RestApi(this, 'fileApi', {
+      restApiName: 'File Processing API',
+
+    });
+    
+const presignLambda = new lambda.Function(this, 'PresignLambda', {
+  runtime: lambda.Runtime.NODEJS_16_X, 
+  handler: 'presignHandler.handler', 
+  code: lambda.Code.fromAsset('lambda'), 
+  environment: {
+    BUCKET_NAME: fileBucket.bucketName,
+  },
+  role: lambdaExecutionRole, 
+});
+
+    const presignedUrl = api.root.addResource('get-presigned-url');
+    presignedUrl.addCorsPreflight({
+      allowHeaders: apigateway.Cors.DEFAULT_HEADERS,
+      allowOrigins: apigateway.Cors.ALL_ORIGINS,
+      allowMethods: apigateway.Cors.ALL_METHODS, 
+    });
+
+    presignedUrl.addMethod('POST', new apigateway.LambdaIntegration(presignLambda));
+
+    const files = api.root.addResource('files');
+
+
+    const vpc = new ec2.Vpc(this, 'VPC'); 
+
   }
 }
+
+
+
+
+
